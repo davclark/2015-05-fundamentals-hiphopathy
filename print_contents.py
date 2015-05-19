a7e559fc8e850df91f50982f277d0f00c4f0e2df
Oski Bear <oski@bear>
2015-05-19 15:41:11 -0700

    Wrapped cleaning in a function

--- print_contents.py
+++ print_contents.py
@@ -1,26 +1,47 @@
 __author__ = 'Dav-Z'
 
-lyrics_file = open("JayZ/JayZ_American Gangster_American Dreamin.txt")
+import glob
+
 # print(lyrics_file.read())
+def clean_lyrics_file(lyrics_file, clean_file):
+    clean_lines = []
 
-# Process file and clean it up
-for line in lyrics_file:
-    line = line.casefold()
-    # Extra Extra credit: get rid of punctuation
-    # Another way to get rid of the chorus junk
-    # line = line.rstrip("[chorus]")
-    line = line.rstrip()
-    for junk_character in [",", "'", "?", "!", "."]:
-        line = line.replace(junk_character, "")
+    # Process file and clean it up
+    for line in lyrics_file:
+        line = line.casefold()
+        # Extra Extra credit: get rid of punctuation
+        # Another way to get rid of the chorus junk
+        # line = line.rstrip("[chorus]")
+        line = line.rstrip()
+        for junk_character in [",", "'", "?", "!", "."]:
+            line = line.replace(junk_character, "")
 
-    found_junk_line = False
-    for junk_line in ["[chorus]", "[chorus:]"]:
-        if junk_line in line:
-            found_junk_line = True
+        found_junk_line = False
+        for junk_line in ["[chorus]", "[chorus:]"]:
+            if junk_line in line:
+                found_junk_line = True
 
-    # if "[chorus" not in line:
-    if not found_junk_line:
-        if line:
-            print(line)
+        # if "[chorus" not in line:
+        if not found_junk_line:
+            if line:
+                clean_file.write(line + '\n')
+                clean_lines.append(line)
+
+
+for infile in glob.glob("JayZ/*.txt"):
+    # stuff below goes here!
+    lyricsf = open(infile)
+    cleanf = open(infile + '.clean', 'w')
+
+    clean_lyrics_file(lyricsf, cleanf)
+
+    # Want to do...
+    # print("We got", len(clean_lines), "lines")
+
+    lyrics_file.close()
+    clean_file.close()
+
+print("We're done!")
 
 # TODO Replace [Chorus] with actual chorus text
+# At the end - example of accumulator
