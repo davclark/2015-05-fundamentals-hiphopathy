e1ff69bd51aceaa541f03a10bbddadf9c8b745ce
Dav Clark <davclark@berkeley.edu>
2015-05-19 13:59:39 -0700

    Added initial for loop

--- print_contents.py
+++ print_contents.py
@@ -1,4 +1,8 @@
 __author__ = 'Dav-Z'
 
-lyrics_file = open("/home/oski/Desktop/Shared/sf_Projects/2015-05-fundamentals-hiphopathy/JayZ/JayZ_American Gangster_American Dreamin.txt")
-print(lyrics_file.read())
+lyrics_file = open("JayZ/JayZ_American Gangster_American Dreamin.txt")
+# print(lyrics_file.read())
+
+for line in lyrics_file:
+    line = line.rstrip()
+    print(line)
