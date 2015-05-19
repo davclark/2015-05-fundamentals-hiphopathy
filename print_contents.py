8ee2ba75e96a845a63ed90afde9c25656f7a5f9c
Oski Bear <oski@bear>
2015-05-19 12:07:40 -0700

    Initial Project

---
+++ print_contents.py
@@ -0,0 +1,4 @@
+__author__ = 'Dav-Z'
+
+lyrics_file = open("/home/oski/Desktop/Shared/sf_Projects/2015-05-fundamentals-hiphopathy/JayZ/JayZ_American Gangster_American Dreamin.txt")
+print(lyrics_file.read())
