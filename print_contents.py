2f1c8c1831931fd22e9e473d14ca979af6fa42de
Oski Bear <oski@bear>
2015-05-19 15:11:18 -0700

    Added nice initial text filtering

--- print_contents.py
+++ print_contents.py
@@ -5,6 +5,22 @@ lyrics_file = open("JayZ/JayZ_American Gangster_American Dreamin.txt")
 
 # Process file and clean it up
 for line in lyrics_file:
+    line = line.casefold()
+    # Extra Extra credit: get rid of punctuation
+    # Another way to get rid of the chorus junk
+    # line = line.rstrip("[chorus]")
     line = line.rstrip()
-    if line:
-        print(line)
+    for junk_character in [",", "'", "?", "!", "."]:
+        line = line.replace(junk_character, "")
+
+    found_junk_line = False
+    for junk_line in ["[chorus]", "[chorus:]"]:
+        if junk_line in line:
+            found_junk_line = True
+
+    # if "[chorus" not in line:
+    if not found_junk_line:
+        if line:
+            print(line)
+
+# TODO Replace [Chorus] with actual chorus text
