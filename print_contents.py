dede04776add651be55811b3a7ce40a2703a1bc1
Oski Bear <oski@bear>
2015-05-19 14:15:58 -0700

    Added skip blank lines logic

--- print_contents.py
+++ print_contents.py
@@ -3,6 +3,8 @@ __author__ = 'Dav-Z'
 lyrics_file = open("JayZ/JayZ_American Gangster_American Dreamin.txt")
 # print(lyrics_file.read())
 
+# Process file and clean it up
 for line in lyrics_file:
     line = line.rstrip()
-    print(line)
+    if line:
+        print(line)
